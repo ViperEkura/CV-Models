b01e68d12cbb9c7c53ed73e02183836f882a29b4
ViperEkura <3081035982@qq.com>
2025-07-25 20:30:54 +0800

    feat(model): 添加匈牙利匹配器和GIOU计算

--- modules/model/detr.py
+++ modules/model/detr.py
@@ -1,7 +1,81 @@
 import torch
 import torch.nn as nn
+import numpy as np
+
+from typing import List, Tuple
 from torch import Tensor
 from modules.model.resnet import ResNet
+from lap import lapjv
+
+def jonker_volgenant(cost_matrix: np.ndarray) -> Tuple[int, List[int], List[int]]:
+    """
+    Jonker-Volgenant算法
+    """
+    assert cost_matrix.ndim == 2
+    return lapjv(cost_matrix)
+
+def _xywh_to_xyxy(boxes: Tensor) -> Tensor:
+    """
+    将边界框的坐标从(x, y, w, h)转换为(x1, y1, x2, y2)
+    """
+    x, y, w, h = boxes.unbind(-1)
+    return torch.stack([x - w/2, y - h/2, x + w/2, y + h/2], dim=-1)
+
+def _box_intersection(boxes1: Tensor, boxes2: Tensor) -> Tensor:
+    """
+    计算两组边界框的交集区域面积
+    """
+    # 取交集坐标
+    x1 = torch.max(boxes1[..., 0], boxes2[..., 0])
+    y1 = torch.max(boxes1[..., 1], boxes2[..., 1])
+    x2 = torch.min(boxes1[..., 2], boxes2[..., 2])
+    y2 = torch.min(boxes1[..., 3], boxes2[..., 3])
+
+    # 计算交集宽高，使用clamp保证非负
+    w = torch.clamp(x2 - x1, min=0)
+    h = torch.clamp(y2 - y1, min=0)
+    
+    return w * h
+
+def _box_union(boxes1: Tensor, boxes2: Tensor) -> Tensor:
+    """
+    计算两组边界框的并集区域面积
+    """
+    area1 = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    area2 = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+    inter = _box_intersection(boxes1, boxes2)
+    return area1 + area2 - inter
+
+def _box_enclose_area(boxes1: Tensor, boxes2: Tensor) -> Tensor:
+    """
+    计算包含两组边界框的最小闭包区域面积
+    """
+    # 取最小外接框坐标
+    x1 = torch.min(boxes1[..., 0], boxes2[..., 0])
+    y1 = torch.min(boxes1[..., 1], boxes2[..., 1])
+    x2 = torch.max(boxes1[..., 2], boxes2[..., 2])
+    y2 = torch.max(boxes1[..., 3], boxes2[..., 3])
+
+    # 计算闭包宽高
+    w = x2 - x1
+    h = y2 - y1
+    
+    return w * h
+
+def _box_giou( boxes1: Tensor, boxes2: Tensor) -> Tensor:
+    """
+    计算两个box之间的GIOU
+    """
+    boxes1 = _xywh_to_xyxy(boxes1)
+    boxes2 = _xywh_to_xyxy(boxes2)
+
+    inter = _box_intersection(boxes1, boxes2)
+    union = _box_union(boxes1, boxes2)
+    iou = inter / union
+
+    enclose = _box_enclose_area(boxes1, boxes2)
+    giou = iou - (enclose - union) / enclose
+    return giou
 
 
 class DETR(nn.Module):
@@ -54,3 +128,59 @@ class DETR(nn.Module):
         pred_bbox = torch.sigmoid(self.linear_bbox(h))                  # [batch_size, num_queries, 4]
     
         return pred_class, pred_bbox
+
+
+class HungarianMatcher(nn.Module):
+    def __init__(
+        self, 
+        cost_class: float = 1.0, 
+        cost_bbox: float = 1.0, 
+        cost_giou: float = 1.0
+    ):
+        super().__init__()
+        self.cost_class = cost_class 
+        self.cost_bbox = cost_bbox 
+        self.cost_giou = cost_giou
+        
+    @torch.no_grad()
+    def forward(
+        self, 
+        pred_class: Tensor, 
+        pred_bbox: Tensor, 
+        gt_class: Tensor, 
+        gt_bbox: Tensor
+    ) -> List:
+        batch_size = pred_class.size(0)
+        indices = []
+        
+        for i in range(batch_size):
+            gt_class_i = gt_class[i]      # [num_gt_boxes]
+            gt_bbox_i = gt_bbox[i]        # [num_gt_boxes, 4]
+
+            cost_class = self._compute_class_cost(pred_class[i], gt_class_i)
+            cost_bbox = self._compute_bbox_cost(pred_bbox[i], gt_bbox_i)
+            cost_giou = self._compute_giou_cost(pred_bbox[i], gt_bbox_i)
+            C = self.cost_class * cost_class + self.cost_bbox * cost_bbox + self.cost_giou * cost_giou
+            C = C.reshape(pred_class.shape[1], -1).cpu().numpy().astype(np.float64)  # [num_queries, num_gt_boxes]
+
+            row_ind, col_ind, _ = jonker_volgenant(C)
+            indices.append((row_ind, col_ind))
+
+        return indices
+    
+    def _compute_class_cost(self, pred_class: Tensor, gt_class: Tensor) -> Tensor:
+        pred_prob = pred_class.softmax(-1)                  # [num_queries, num_classes+1]
+        cost_class = -pred_prob[:, gt_class]                # [num_queries, num_gt_boxes]
+        return cost_class
+    
+    def _compute_bbox_cost(self, pred_bbox, gt_bbox) -> Tensor:
+        cost_bbox = torch.cdist(pred_bbox, gt_bbox, p=1)    # [num_queries, num_gt_boxes]
+        return cost_bbox
+    
+    def _compute_giou_cost(self, pred_bbox: Tensor, gt_bbox: Tensor):
+        giou = _box_giou(pred_bbox, gt_bbox)                # [num_queries, num_gt_boxes]
+        cost_giou = 1 - giou
+        return cost_giou
+
+
+
