15f56085a6494b2d96aea7d93e27bc0150c299c3
ViperEkura <3081035982@qq.com>
2025-07-25 20:56:10 +0800

    feat(model): 修改__init__.py

--- modules/model/__init__.py
+++ modules/model/__init__.py
@@ -1,9 +1,15 @@
-from .detr import DETR
+from .detr import DETR, HungarianMatcher
 from .motr import MOTR
 from .resnet import ResNet
 
 __all__ = [
+    # detr
     "DETR",
+    "HungarianMatcher",
+    
+    # motr
     "MOTR",
+    
+    # resnet
     "ResNet"
 ]
