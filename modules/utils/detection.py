4ea7e94dcb10ad5cc8bdcf7c65cc19e50b62d709
ViperEkura <3081035982@qq.com>
2025-08-05 12:38:18 +0800

    feat(utils): 添加模型训练和测试函数

--- modules/utils/detection.py
+++ modules/utils/detection.py
@@ -1,6 +1,7 @@
 import torch
 
 from torch import Tensor
+from torch.nn import Module
 from typing import Callable, Tuple, List
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
@@ -8,27 +9,63 @@ from torch.utils.data import DataLoader
 
 
 def train_fn(
-    model: Callable[[Tensor],  Tensor],
+    model: Module | Callable[..., Tensor],
     train_loader: DataLoader, 
     optimizer: Optimizer, 
-    criterion: Callable[[Tensor, Tensor],  Tensor], 
+    criterion: Callable[..., Tensor] , 
     epoch: int,
     device: torch.device,
-):
-    pass
+    print_every: int
+) -> float:
+    model.train()
+    loss_list = []
+    for batch_idx, (img, label, box) in enumerate(train_loader):
+        img = img.to(device)
+        label = label.to(device)
+        box = box.to(device)
+        
+        output = model(img)
+        loss = criterion(output, label, box)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        loss_list.append(loss.item())
+        
+        if batch_idx % print_every == 0:
+            epoch_info = f"{epoch} [{batch_idx}/{len(train_loader)}]"
+            iteration_info = f'\nEpoch: {epoch_info} | Loss: {loss.item():.5f}'
+            print(iteration_info)
+    
+    avg_loss = sum(loss_list) / len(loss_list)
+    
+    return avg_loss
 
 
 def test_fn(
-    model: Callable[[Tensor],  Tensor],
+    model: Module | Callable[..., Tensor],
     test_loader: DataLoader,
-    criterion: Callable[[Tensor, Tensor],  Tensor],
+    criterion: Callable[...,  Tensor],
     device: torch.device,
-) -> Tuple[float, float]:
-    pass
+) -> float:
+    model.eval()
+    loss_list = []
+    for _, (img, label, box) in enumerate(test_loader):
+        img = img.to(device)
+        label = label.to(device)
+        box = box.to(device)
+        
+        output = model(img)
+        loss = criterion(output, label, box)
+        loss_list.append(loss.item())
+    
+    avg_loss = sum(loss_list) / len(loss_list)
+    print(f"Test | Average loss: {avg_loss :.4f}")
+    
+    return avg_loss
 
 
 def train_loop(
-    model: Callable[[Tensor],  Tensor],
+    model: Module | Callable[..., Tensor],
     train_loader: DataLoader,
     test_loader: DataLoader,
     optimizer: Optimizer,
@@ -36,5 +73,13 @@ def train_loop(
     epochs: int,
     print_every: int,
     device: torch.device,
-)-> Tuple[List[float], List[float], List[float], List[float]]:
-    pass
+)-> Tuple[List[float], List[float]]:
+    train_losses, test_losses = [], []
+    for epoch in range(1, epochs + 1):
+        train_loss = train_fn(model, train_loader, optimizer, criterion, epoch, device, print_every)
+        test_loss = test_fn(model, test_loader, criterion, device)
+        
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        
+    return train_losses, test_losses
