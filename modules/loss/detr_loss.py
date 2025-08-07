cdb2481b8ca5807d21413f561adaaaf8a395b9c3
ViperEkura <3081035982@qq.com>
2025-08-07 11:20:48 +0800

    fix(loss): 修复 DETR 损失计算代码

--- modules/loss/detr_loss.py
+++ modules/loss/detr_loss.py
@@ -42,15 +42,16 @@ class SetCriterion(Module):
 
         row_inds, col_inds = self.matcher.match(pred_class, pred_bbox, gt_class, gt_bbox)
         self.empty_weight = self.empty_weight.to(pred_class.device)
-        # col_inds: [batch_size, num_queries], col_inds: [batch_size, num_gt_boxes]
+        # rol_inds: [batch_size, num_queries], col_inds: [batch_size, num_gt_boxes]
         
         # 1. class loss compute
         gt_class_expanded = torch.full(
             (batch_size, num_queries),
-            self.num_classes, 
-            device=gt_class.device
+            fill_value=0, 
+            device=gt_class.device,
+            dtype=gt_class.dtype
         )
-        gt_class_expanded = gt_class_expanded.scatter(dim=-1, index=col_inds, src=gt_class)
+        gt_class_expanded = gt_class_expanded.scatter(dim=1, index=col_inds, src=gt_class)
         
         class_loss = F.cross_entropy(
             pred_class.flatten(0, 1),           # [batch_size * num_queries, num_classes + 1]
@@ -59,21 +60,25 @@ class SetCriterion(Module):
         )
         
         # 2. compute for bbox
-        select_mask = row_inds.not_equal(-1)
-        pred_bbox_selected = pred_bbox[select_mask] # [batch_size * num_queries, 4]
-        row_inds_selected = row_inds[select_mask]   # [batch_size * num_queries]
+        gt_bbox_expanded = torch.full(
+            (batch_size, num_queries, 4),
+            fill_value=0, 
+            device=gt_bbox.device,
+            dtype=gt_bbox.dtype
+        )
+        expand_col_inds = col_inds.unsqueeze(-1).expand(-1, -1, 4)
+        gt_bbox_expanded = gt_bbox_expanded.scatter(dim=1, index=expand_col_inds, src=gt_bbox)
         
-        gt_bbox_selected = gt_bbox.flatten(0, 1)[row_inds_selected]
-        gt_class_selected = gt_class.flatten(0, 1)[row_inds_selected]
+        match_mask = row_inds.eq(-1).unsqueeze(-1)
+        obj_mask = gt_class_expanded.eq(0).unsqueeze(-1)
         
-        obj_mask = gt_class_selected .not_equal(0)
-        pred_bbox_selected = pred_bbox_selected[obj_mask]
-        gt_bbox_selected = gt_bbox_selected[obj_mask]
-        
-        bbox_loss = F.l1_loss(pred_bbox_selected, gt_bbox_selected, reduction="mean")
+        # [batch_size, num_queries, 4]
+        pred_bbox_matched = torch.masked_fill(input=pred_bbox, mask=match_mask | obj_mask, value=0)
+        gt_bbox_matched = torch.masked_fill(input=gt_bbox_expanded, mask=match_mask, value=0)
+        bbox_loss = F.l1_loss(pred_bbox_matched.flatten(), gt_bbox_matched.flatten(), reduction="none").sum() / obj_mask.sum().clamp(min=1)
         
         # 3. compute for giou
-        box_giou = _box_giou(pred_bbox_selected, gt_bbox_selected)
+        box_giou = _box_giou(pred_bbox_matched, gt_bbox_matched)
         giou_loss = 1 - torch.diag(box_giou).mean()
 
         total_loss = (
